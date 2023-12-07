c297f50b4ef5f03a7b071daa00d9d13b100665e3
eunsung <esk114477@gmail.com>
2023-12-07 12:13:35 +0900

    feat: insertHouseJSON service 작성

--- Dungjip/src/main/java/com/kh/dungjip/house/model/service/HouseService.java
+++ Dungjip/src/main/java/com/kh/dungjip/house/model/service/HouseService.java
@@ -1,5 +1,9 @@
 package com.kh.dungjip.house.model.service;
 
-//public interface HouseService {
-//
-//}
+import com.kh.dungjip.house.model.vo.House;
+
+public interface HouseService {
+
+	int insertHouseJSON(House house);
+
+}
