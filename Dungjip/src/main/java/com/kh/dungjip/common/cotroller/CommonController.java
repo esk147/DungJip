3a93c72ad9145b5a70fee4f1a41a0d29e202e1b7
eunsung <esk114477@gmail.com>
2023-12-20 11:51:28 +0900

    feat: estate subscribe 기능 추가

--- Dungjip/src/main/java/com/kh/dungjip/common/cotroller/CommonController.java
+++ Dungjip/src/main/java/com/kh/dungjip/common/cotroller/CommonController.java
@@ -1,8 +1,15 @@
 package com.kh.dungjip.common.cotroller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-
-
+@Controller
 public class CommonController {
-
+	
+	@GetMapping("subscribe.sb")
+	public String subscribePage() {
+		
+		return "subscribe/subscribeView";
+	}
+	
 }
