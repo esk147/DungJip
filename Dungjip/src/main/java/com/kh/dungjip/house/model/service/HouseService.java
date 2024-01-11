320c77044121d8053201a05207f88d68a4807ddf
eunsung <esk114477@gmail.com>
2024-01-11 11:31:15 +0900

    feat: 매물 리뷰 공감 기능 추가

--- Dungjip/src/main/java/com/kh/dungjip/house/model/service/HouseService.java
+++ Dungjip/src/main/java/com/kh/dungjip/house/model/service/HouseService.java
@@ -137,6 +137,16 @@ public interface HouseService {
 
 	int deleteKeywords(Map<String, Object> paramMap);
 
+	int selectResidentEmoCount(int reReviewNo);
+
+	int selectResidentReviewLikeCount(Map<String, Object> numMap);
+
+	int selectReviewLikeCount(Map<String, Object> map);
+
+	int decreaseCount(Map<String, Object> map);
+
+	int increaseReReLikeCount(Map<String, Object> map);
+
 
 
 }
